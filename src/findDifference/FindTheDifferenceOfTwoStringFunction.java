be19799ae499a2896dd5b8b756c44e7e8e525901
chaninkou <michaelkoulove@yahoo.com>
2020-04-05 15:55:58 -0700

    Redid problem on using int[26] or ascii or hashmap.

--- src/findDifference/FindTheDifferenceOfTwoStringFunction.java
+++ src/findDifference/FindTheDifferenceOfTwoStringFunction.java
@@ -3,7 +3,52 @@ package findDifference;
 import java.util.HashMap;
 
 public class FindTheDifferenceOfTwoStringFunction {
+	// Fastest way to do it
     public char findTheDifference(String s, String t) {
+        char[] sArray = s.toCharArray();
+        
+        char[] tArray = t.toCharArray();
+        
+        int ascis = 0;
+        int ascit = 0;
+        
+        for(int i = 0; i < sArray.length; i++){
+            ascis += (sArray[i] - 'a');
+        }
+        
+        for(int j = 0; j < tArray.length; j++){
+            ascit += (tArray[j] - 'a');
+        }
+        
+        // If the answer is 4, which is e, just add back the 'a'
+        return (char) (ascit - ascis + 'a');
+    }
+    
+	// Since we know its only lowercase, using an array is fine
+    public char findTheDifference1(String s, String t) {
+        if(t.length() != s.length() + 1){
+            return '?';
+        }
+        
+        int[] map = new int[26];
+        
+        for(int i = 0; i < s.length(); i++){
+            map[s.charAt(i) - 'a']++;
+        }
+        
+        for(int j = 0; j < t.length(); j++){
+            map[t.charAt(j) - 'a']--;
+            
+            // Whichever value is negative will be the missing value
+            if(map[t.charAt(j) - 'a'] < 0){
+                return t.charAt(j);
+            }
+        }
+        
+        return '?';
+    }
+    
+    public char findTheDifference2(String s, String t) {
         HashMap<Character, Integer> map = new HashMap<>();
         
         // Go through the original string and count how many times it appear
